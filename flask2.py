b99fdfab7996f9d4308d34d6d0c42d424a2b286b
Shambhavi Singh <shambhavisingh428@gmail.com>
2025-04-02 15:25:31 +0530

    Initial commit

--- flask2.py
+++ flask2.py
@@ -55,5 +55,6 @@ def update(sno):
     todo= Todo.query.filter_by(sno=sno).first()
     return render_template('update.html',todo=todo)
 
-if __name__== '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
